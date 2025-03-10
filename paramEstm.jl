a9103d7396622e8f7d82d3945822db3f507a11f0
jinli <jinli.stat@hotmail.com>
2025-03-10 17:25:26 +0800

    tuning parameters

--- paramEstm.jl
+++ paramEstm.jl
@@ -1,97 +1,184 @@
-using Optimization
-using OptimizationNLopt
-using Statistics
-include("liklhdFun.jl")
+using Optimization, OptimizationNLopt, ForwardDiff
+using Statistics, LinearAlgebra
 
-function pen(x)
-    abs(x)
+include("liklhdFun.jl")
+function scad_penalty(beta_hat, tuning_param)
+    a_val = 3.7
+    if abs.(beta_hat) <= tuning_param
+        return tuning_param * abs(beta_hat)
+    elseif tuning_param < abs(beta_hat) <= a_val * tuning_param
+        return (2 * a_val * tuning_param * abs(beta_hat) - beta_hat^2 - tuning_param^2) / (2 * (a_val - 1))
+    else
+        return (tuning_param^2 * (a_val + 1)) / 2
+    end
 end
 
-function local_estimator(data, mu_initial, gamma_initial, knots; order=3)
+function scad_derivative(beta_hat, tuning_param)
+    a_val = 3.7
+    return tuning_param * (
+        (beta_hat <= tuning_param) +
+        ((a_val * tuning_param - beta_hat) * ((a_val * tuning_param - beta_hat) > 0)) / ((a_val - 1) * tuning_param) *
+        (beta_hat > tuning_param)
+    )
+end
+
+function scad_quadratic_approx(beta_hat, beta_0, tuning_param)
+    scad_penalty(beta_0, tuning_param) + 0.5*scad_derivative(beta_0, tuning_param)*(beta_hat^2 - beta_0^2)/(abs(beta_0) + 1e-10)
+end
+
+function BayIC(data_reorgnz_val, mu_val, alpha_val, gamma_val, n)
+    lkhd = logliklhd_k(mu_val, alpha_val, gamma_val, data_reorgnz_val)
+    non_zero_mu_count = count(x -> x != 0, mu_val)
+    return - 2*lkhd + non_zero_mu_count*log(n)
+end
+
+function DF_val(data_reorgnz_val, mu_val, alpha_val, gamma_val, n)
+    logliklhd_all = mapreduce(+, 1:k) do i
+        logliklhd_k(mu_val, alpha_val[i,:], gamma_val, data_reorgnz_val[i])
+    end
+    non_zero_mu_count = count(x -> x != 0, mu_val)
+    non_zero_alpha_count = count(x -> x != 0, vec(alpha_val[2:end,:]))
+    return - 2*logliklhd_all + (non_zero_mu_count + non_zero_alpha_count)*log(n)
+end
+
+struct optimization_result
+    xi_1::Float64
+    xi_2::Float64
+    criterion::Float64
+    mu::Vector{Float64}
+    alpha::AbstractArray{Float64}
+    gamma::Vector{Float64}
+end
+
+function local_estimator(data, mu_initial, gamma_initial, knots; spl_order=3, penalty = false)
     alpha = zeros(size(mu_initial))
     n = size(data, 1)
     p = size(mu_initial, 1)
-    data_reorgnz = PIC_data_reorgnz(data, order, knots)
+    data_reorgnz = PIC_data_reorgnz(data, spl_order, knots)
+    xi = 0.01
 
     function object_fun(vars, fixed_values)
         mu = vars[1:p]
         gamma = vars[p+1:end]
-        alpha, data_reorgnz = fixed_values
-        return -logliklhd_k(mu, alpha, gamma, data_reorgnz) + (n * 0.01 * sum(pen.(mu)))
+        alpha, data_reorgnz, xi = fixed_values
+        if penalty == true
+            return -logliklhd_k(mu, alpha, gamma, data_reorgnz) + n * sum(scad_quadratic_approx.(mu, mu_initial, xi))
+        else
+            return -logliklhd_k(mu, alpha, gamma, data_reorgnz)
+        end
     end
-    x0 = vcat(mu_initial, gamma_initial)
-    f = OptimizationFunction(object_fun, Optimization.AutoForwardDiff())
-    lb = vcat(fill(-Inf, p), fill(0, size(gamma_initial)))
-    ub = vcat(fill(Inf, p), fill(Inf, size(gamma_initial)))
-    prob = OptimizationProblem(f, x0, (alpha, data_reorgnz), lb = lb, ub = ub)
 
-    sol = solve(
-        prob, 
-        NLopt.LD_LBFGS(),
-        stopval = 1e-5,
-        ftol_rel = 1e-5,
-        xtol_abs = 1e-5, 
-        maxeval = 1000)
-    optimized_mu = sol.u[1:p]
-    optimized_mu[abs.(optimized_mu).<=0.01].=0.0
-    optimized_gamma = sol.u[p+1:end]
+    function evaluate_tuning_param(xi)
+        x0 = vcat(mu_initial, gamma_initial)
+        f = OptimizationFunction(object_fun, Optimization.AutoForwardDiff())
+        lb = vcat(fill(-10, p), fill(0, size(gamma_initial)))
+        ub = vcat(fill(10, p), fill(10, size(gamma_initial)))
+        prob = OptimizationProblem(f, x0, (alpha, data_reorgnz, xi), lb = lb, ub = ub)
 
-    return optimized_mu, optimized_gamma
+        sol = solve(prob, 
+            NLopt.LD_LBFGS(), 
+            stopval = 1e-5, 
+            ftol_rel = 1e-5, 
+            xtol_abs = 1e-5, 
+            maxeval = 1000)
+        mu_hat = sol.u[1:p]
+        mu_hat[abs.(mu_hat).<=0.01] .= 0.0
+        gamma_hat = sol.u[p+1:end]
+        BIC_val = BayIC(data_reorgnz, mu_hat, alpha, gamma_hat, n)
+
+        return optimization_result(xi, xi, BIC_val, mu_hat, alpha, gamma_hat)
+    end
+    if penalty == false
+        this_result = evaluate_tuning_param(1.0)
+        return this_result.mu, this_result.gamma
+    end
+    
+    param_grid = [0.0001,0.001, 0.005, 0.01, 0.03, 0.05, 0.07, 0.1, 0.3, 0.5, 0.7, 1.0]
+    n_combinations = length(param_grid)
+    tuning_results = Vector{optimization_result}(undef, n_combinations)
+    for i in 1:n_combinations
+        xi_val = param_grid[i]
+        tuning_results[i] = evaluate_tuning_param(xi_val)
+    end
+    best_idx = argmin(map(r -> r.criterion, tuning_results))
+    best_result = tuning_results[best_idx]
+
+    return best_result.mu, best_result.gamma
 end
 
-function multisource_estimator(data, mu_initial, gamma_initial, knots; order=3)
+function multisource_estimator(data, mu_initial, alpha_initial, gamma_initial, knots; spl_order=3, penalty = false)
     p = size(mu_initial, 1)
     n = sum(size(df, 1) for df in data)
     data_reorgnz = ()
-    k = length(data)
-    # alpha_initial = vcat(fill(0.0, p), fill(1.0, p), fill(2.0, p), fill(3.0, p))
-    alpha_initial = fill(0.0, p*k)
-    data_reorgnz = ntuple(i -> PIC_data_reorgnz(data[i], order, knots), k)
+    k = length(data) 
+    data_reorgnz = ntuple(i -> PIC_data_reorgnz(data[i], spl_order, knots), k)
 
-    tun_1 = tun_2 = 0.01
-    function object_fun(vars, fixed_values)
+    function object_fun(vars, fixed_val)
         mu = @view vars[1:p]
         gamma = @view vars[(k+1)*p+1:end]
         alpha_flat = @view vars[p+1:p*(k+1)]
         alpha_mat = reshape(alpha_flat, p, k)'
-
+        fixed_values, xi_1, xi_2 = fixed_val
         logliklhd_all = mapreduce(+, 1:k) do i
             logliklhd_k(mu, alpha_mat[i,:], gamma, fixed_values[i])
         end
-
-        pen_1 = n * tun_1 * sum(pen.(mu))
-        pen_2 = n * tun_2 * mapreduce(x -> pen(norm(x)), +, eachcol(alpha_mat))
         
-        return - logliklhd_all + pen_1 + pen_2
+        if penalty == true
+            pen_1 = n * sum(scad_quadratic_approx.(mu, mu_initial, xi_1))
+            alpha_alpha_initial_norm = map(norm, eachcol(alpha_initial))
+            alpha_norm = map(norm, eachcol(alpha_mat))
+            pen_2 = n * sum(scad_quadratic_approx.(alpha_norm, alpha_alpha_initial_norm, xi_2))
+            return -logliklhd_all + pen_1 + pen_2
+        else
+            return -logliklhd_all
+        end
     end
 
-    x0 = vcat(mu_initial, alpha_initial, gamma_initial)
-    lb = vcat(fill(-10, p*(k+1)), fill(0, size(gamma_initial)))
-    ub = vcat(fill(10, p*(k+1)), fill(10, size(gamma_initial)))
+    function evaluate_tuning_param(xi_1, xi_2)
+        x0 = vcat(mu_initial, alpha_initial, gamma_initial)
+        f = OptimizationFunction(object_fun, Optimization.AutoForwardDiff())
+        lb = vcat(fill(-10, p*(k+1)), fill(0, size(gamma_initial)))
+        ub = vcat(fill(10, p*(k+1)), fill(10, size(gamma_initial)))
+        prob = OptimizationProblem(f, x0, (data_reorgnz, xi_1, xi_2), lb = lb, ub = ub)
+
+        sol = solve(
+            prob, 
+            NLopt.LD_LBFGS(),
+            stopval = 1e-5, 
+            ftol_rel = 1e-5,
+            xtol_abs = 1e-5, 
+            maxeval = 1000,
+            maxtime = 30)
+        
+        mu_hat = sol.u[1:p]
+        mu_hat[abs.(mu_hat).<=0.01].=0.0
+        alpha_hat = [sol.u[p*i+1:p*(i+1)] for i in 1:k]
+        alpha_hat = reshape(sol.u[p+1:p*(k+1)], p, k)'
+        alpha_hat = alpha_hat .- mean(alpha_hat, dims=1)
+        alpha_hat[abs.(alpha_hat).<=0.01].=0.0
+        gamma_hat = sol.u[(k+1)*p+1:end]
+        DF = DF_val(data_reorgnz, mu_hat, alpha_hat, gamma_hat, n)
+        return optimization_result(xi_1, xi_2, DF, mu_hat, alpha_hat, gamma_hat)
+    end
+
+    if penalty == false
+        this_result = evaluate_tuning_param(1.0, 1.0)
+        return this_result.mu, this_result.alpha ,this_result.gamma
+    end
+
+    param1 = [0.0001,0.001, 0.005, 0.01, 0.03, 0.05, 0.07, 0.1, 0.3, 0.5, 0.7, 1.0]
+    param2 = [0.0001,0.001, 0.005, 0.01, 0.03, 0.05, 0.07, 0.1, 0.3, 0.5, 0.7, 1.0]
+    param_grid = collect(Base.Iterators.product(param1, param2)) |> vec
+    n_combinations = length(param_grid)
+    tuning_results = Vector{optimization_result}(undef, n_combinations)
+    for i in 1:n_combinations
+        xi_1, xi_2 = param_grid[i]
+        tuning_results[i] = evaluate_tuning_param(xi_1, xi_2)
+    end
     
-    f = OptimizationFunction(object_fun, Optimization.AutoForwardDiff())
+    best_idx = argmin(map(r -> r.criterion, tuning_results))
+    println("Tuning parameters: ", param_grid[best_idx], " Criterion: ", tuning_results[best_idx].criterion)
+    best_result = tuning_results[best_idx]
 
-    prob = OptimizationProblem(f, x0, data_reorgnz, lb = lb, ub = ub)
-
-    sol = solve(
-        prob, 
-        NLopt.LD_LBFGS(),
-        stopval = 1e-5, 
-        ftol_rel = 1e-5,
-        xtol_abs = 1e-5, 
-        maxeval = 100,
-        maxtime = 30)
-    
-    optimized_mu = sol.u[1:p]
-    optimized_mu[abs.(optimized_mu).<=0.01].=0.0
-    optimized_alpha = [sol.u[p*i+1:p*(i+1)] for i in 1:k]
-    optimized_alpha = reshape(sol.u[p+1:p*(k+1)], p, k)'
-    optimized_alpha = optimized_alpha .- mean(optimized_alpha, dims=1)
-    optimized_alpha[abs.(optimized_alpha).<=0.01].=0.0
-    optimized_gamma = sol.u[(k+1)*p+1:end]
-
-    return optimized_mu, optimized_alpha, optimized_gamma
+    return best_result.mu, best_result.alpha, best_result.gamma
 end
-
-optimized_mu, optimized_alpha, optimized_gamma = multisource_estimator(data, mu_initial, gamma_initial, knots; order=3)
-optimized_alpha .+ optimized_mu'
--- test.jl
+++ test.jl
@@ -27,17 +27,51 @@ data_3 = gen_pic_data(n, 0.2, beta_3, "case1")
 data_4 = gen_pic_data(n, 0.2, beta_4, "case1")
 
 data = (data_1, data_2, data_3, data_4)
+
+
+
+J0 = 10
+p =  10
+n = 1000
+real_mu = vcat(-0.5, -0.5, 0.5, 0.5, 0.0, 0.0, fill(0.0, p-6))
+alpha_1 = vcat(0.0, 0.0, 0.5, 0.5, 0.5, 0.5, fill(0.0, p-6))
+alpha_2 = vcat(0.0, 0.0, -0.5, -0.5, -0.5, -0.5, fill(0.0, p-6))
+alpha_3 = vcat(0.0, 0.0, -0.5, -0.5, -0.5, -0.5, fill(0.0, p-6))
+alpha_4 = vcat(0.0, 0.0, 0.5, 0.5, 0.5, 0.5, fill(0.0, p-6))
+beta_1 = real_mu + alpha_1
+beta_2 = real_mu + alpha_2
+beta_3 = real_mu + alpha_3
+beta_4 = real_mu + alpha_4
+@elapsed begin
+    for s in 1:100
+        Random.seed!(s)
+        data_1 = gen_pic_data(n, 0.2, beta_1, "case1")
+        knots =  initial_knots(union(data_1[:,1], data_1[:,2], 0.0, Inf), J0)[1:end-1]
+        spl_order = 3
+        mu_initial = fill(0.0, p)
+        gamma_initial = fill(0.1, J0-2+spl_order)
+        mu_0, gamma_0 = local_estimator(data_1, mu_initial, gamma_initial, knots; spl_order=spl_order)
+        mu_1, gamma_1 = local_estimator(data_1, mu_0, gamma_0, knots; spl_order = spl_order, penalty = true)
+    end
+end
+
+
+
+Random.seed!(2025)
+data_1 = gen_pic_data(n, 0.2, beta_1, "case1")
+data_2 = gen_pic_data(n, 0.2, beta_2, "case1")
+data_3 = gen_pic_data(n, 0.2, beta_3, "case1")
+data_4 = gen_pic_data(n, 0.2, beta_4, "case1")
+
+data = (data_1, data_2, data_3, data_4)
 data_full = vcat(data_1, data_2, data_3, data_4)
-length(data)
+k=length(data)
+alpha_initial = fill(0.0, p*k)
 knots =  initial_knots(union(data_full[:,1], data_full[:,2], 0.0, Inf), J0)[1:end-1]
-
-order = 3
 mu_initial = fill(0.0, p)
-gamma_initial = fill(0.01, J0-2+order)
-
-optimized_mu, optimized_gamma = local_estimator(data_full, mu_initial, gamma_initial, knots; order=order)
-optimized_mu
-optimized_gamma
-
-
-
+spl_order = 3
+gamma_initial = fill(0.1, J0-2 + spl_order)
+mu_0, alpha_0, gamma_0 = multisource_estimator(data, mu_initial, alpha_initial, gamma_initial, knots; spl_order=3, penalty = false)
+alpha_0 = vec(alpha_0')
+mu_1, alpha_1, gamma_1 = multisource_estimator(data, mu_0, alpha_0, gamma_0, knots; spl_order=3, penalty = true)
+mu_1' .+ alpha_1
